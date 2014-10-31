4e744f0c62e94c0b1d091e00b12a69a6bbbbca3d
Nikusiaczek <agnieszka.wesolowska1@gmail.com>
2014-10-31 14:50:52 +0100

    wrzucenie testowe

---
+++ PopulationRegistering/PopulationRegistering/ViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PopulationRegistering
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        #region Fields
+        public event PropertyChangedEventHandler PropertyChanged = null;
+        #endregion
+
+        #region Methods
+        virtual protected void OnPropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+        #endregion
+    }
+}
